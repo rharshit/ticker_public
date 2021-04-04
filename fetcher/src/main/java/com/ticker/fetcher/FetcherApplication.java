0d506af6d765abdfe8faf87381b8a88c5e408958
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-04-04 13:59:50 +0530

    Fetcher: Initialize

---
+++ fetcher/src/main/java/com/ticker/fetcher/FetcherApplication.java
@@ -0,0 +1,13 @@
+package com.ticker.fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetcherApplication.class, args);
+	}
+
+}
