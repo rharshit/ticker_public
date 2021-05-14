d030d53e7e662229b617519ac9a7ad4bbceccff3
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-05-14 22:37:35 +0530

    Brokerage: Implement webdriver

--- brokerage/src/main/java/com/ticker/brokerage/service/BrokerageService.java
+++ brokerage/src/main/java/com/ticker/brokerage/service/BrokerageService.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-
 import static com.ticker.brokerage.common.constants.WebConstants.ZERODHA_BROKERAGE_URL;
 
 
@@ -15,32 +13,22 @@ import static com.ticker.brokerage.common.constants.WebConstants.ZERODHA_BROKERA
 @Slf4j
 public class BrokerageService {
 
-    private static WebDriver webDriver;
+    private static final WebDriver webDriver;
+
+    static {
+        log.info("Initializing webdriver");
+        ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("incognito");
+        webDriver = new ChromeDriver(options);
+        webDriver.get(ZERODHA_BROKERAGE_URL);
+        log.info("Webdriver initialized");
+    }
 
     public void getZerodhaBrokerage(String type) {
+        synchronized (webDriver) {
 
-    }
-
-    @PostConstruct
-    public void init() {
-        log.info("Initializing BrokerageService");
-        initWebdriver();
-        log.info("BrokerageService initialized");
-    }
-
-    private void initWebdriver() {
-        if (webDriver == null) {
-            log.info("Initializing webdriver");
-            ChromeOptions options = new ChromeOptions();
-            //options.setHeadless(true);
-            options.addArguments("--window-size=1920,1080");
-            options.addArguments("incognito");
-            webDriver = new ChromeDriver(options);
-            webDriver.get(ZERODHA_BROKERAGE_URL);
-            log.info("Webdriver initialized");
-        } else {
-            log.info("Webdriver already initialized");
         }
-
     }
 }
