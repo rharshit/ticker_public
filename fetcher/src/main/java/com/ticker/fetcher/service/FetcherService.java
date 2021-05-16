8b72951c35fe6c782ffd2559f7671245c2b69c07
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-05-17 03:25:26 +0530

    Fetcher: Optimize page loading

--- fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
+++ fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
@@ -1,5 +1,6 @@
 package com.ticker.fetcher.service;
 
+import com.ticker.fetcher.common.exception.TickerException;
 import com.ticker.fetcher.common.rx.FetcherThread;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
@@ -27,15 +28,10 @@ public class FetcherService {
      */
     public void setChartSettings(WebDriver webDriver, int iteration) {
         int iterationMultiplier = 200;
-        //spinner loading-indicator
-        waitTillLoad(webDriver, WAIT_SHORT + iteration*iterationMultiplier, 2);
-
         // Chart style
-        waitTillLoad(webDriver, WAIT_SHORT + iteration*iterationMultiplier, 2);
         configureMenuByValue(webDriver, "menu-inner", "header-toolbar-chart-styles", "ha");
 
         // Chart interval
-        waitTillLoad(webDriver, WAIT_SHORT + iteration*iterationMultiplier, 2);
         configureMenuByValue(webDriver, "menu-inner", "header-toolbar-intervals", "1");
 
         // Indicators
@@ -50,6 +46,7 @@ public class FetcherService {
      * @param threshold
      */
     private void waitTillLoad(WebDriver webDriver, long time, int threshold) {
+        waitFor(WAIT_SHORT);
         while (webDriver.findElements(By.cssSelector("div[role='progressbar']")).size() > threshold);
         waitFor(time);
     }
@@ -57,35 +54,54 @@ public class FetcherService {
     private void setIndicators(WebDriver webDriver, String... indicators) {
         WebElement chartStyle = webDriver.findElement(By.id("header-toolbar-indicators"));
         chartStyle.click();
-        waitTillLoad(webDriver, WAIT_SHORT, 2);
-        WebElement menuBox = webDriver
-                .findElement(By.id("overlap-manager-root"))
+        WebElement overLapManagerRoot = webDriver
+                .findElement(By.id("overlap-manager-root"));
+        while (CollectionUtils.isEmpty(overLapManagerRoot
+                .findElements(By.cssSelector("div[data-name='indicators-dialog']")))) ;
+        WebElement menuBox = overLapManagerRoot
                 .findElement(By.cssSelector("div[data-name='indicators-dialog']"));
 
-        waitTillLoad(webDriver, WAIT_SHORT, 2);
-
         for (String indicator : indicators) {
-            String searchText = indicator.split(":")[0];
-            WebElement searchBox = menuBox.findElement(By.cssSelector("input[data-role='search']"));
-            searchBox.click();
-            searchBox.sendKeys(searchText);
-            waitTillLoad(webDriver, WAIT_SHORT, 2);
-
-            String indicatorId = indicator.split(":")[1];
-            WebElement valueElement = menuBox.findElement(By.cssSelector("div[data-id='" + indicatorId + "']"));
-            valueElement.click();
-
-            searchBox.sendKeys(Keys.BACK_SPACE);
-            waitTillLoad(webDriver, WAIT_SHORT, 2);
+            selectIndicator(indicator, menuBox, 0);
         }
-        WebElement closeBtn = webDriver
-                .findElement(By.id("overlap-manager-root"))
+        WebElement closeBtn = overLapManagerRoot
                 .findElement(By.cssSelector("span[data-name='close']"))
                 .findElement(By.tagName("svg"));
         closeBtn.click();
     }
 
+    private void selectIndicator(String indicator, WebElement menuBox, int iteration) {
+        int numRetries = 5;
+        log.info("Selecting indicator " + indicator);
+        try {
+            String searchText = indicator.split(":")[0];
+            WebElement searchBox = menuBox.findElement(By.cssSelector("input[data-role='search']"));
+            searchBox.click();
+            searchBox.sendKeys(Keys.COMMAND + "a");
+            searchBox.sendKeys(searchText);
+            while (menuBox.findElement(By.cssSelector("div[data-role='dialog-content']")).findElements(By.cssSelector("div[data-role='list-item']")).size() <= 3)
+                ;
+            String indicatorId = indicator.split(":")[1];
+            menuBox.findElement(By.cssSelector("div[data-id='" + indicatorId + "']")).click();
+
+            searchBox.sendKeys(Keys.BACK_SPACE);
+            while (menuBox.findElement(By.cssSelector("div[data-role='dialog-content']")).findElements(By.cssSelector("div[data-role='list-item']")).size() <= 3)
+                ;
+        } catch (Exception e) {
+            iteration = iteration + 1;
+            log.error("Iteration " + iteration + " failed");
+            if (iteration < numRetries) {
+                selectIndicator(indicator, menuBox, iteration);
+            } else {
+                e.printStackTrace();
+                throw new TickerException("Cannot select indicator " + indicator);
+            }
+        }
+        log.info("Selected indicator " + indicator);
+    }
+
     private void configureMenuByValue(WebDriver webDriver, String dataName, String header, String value) {
+        while (CollectionUtils.isEmpty(webDriver.findElements(By.id(header)))) ;
         WebElement chartStyle = webDriver.findElement(By.id(header));
         chartStyle.click();
         WebElement menuBox = webDriver
