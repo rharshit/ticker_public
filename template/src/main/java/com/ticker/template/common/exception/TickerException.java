27f71aad573729e018e08e25f4c67427b06621b8
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-20 01:12:21 +0530

    Add Template

---
+++ template/src/main/java/com/ticker/template/common/exception/TickerException.java
@@ -0,0 +1,7 @@
+package com.ticker.template.common.exception;
+
+public class TickerException extends RuntimeException {
+    public TickerException(String message) {
+        super(message);
+    }
+}
---
+++ template/src/test/java/com/ticker/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticker.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
