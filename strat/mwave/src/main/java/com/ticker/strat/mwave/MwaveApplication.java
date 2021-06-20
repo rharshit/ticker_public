faf92eb012a407f5de187ded1688853f9acb83d1
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-20 14:54:38 +0530

    M-Wave: Initialize

---
+++ strat/mwave/src/main/java/com/ticker/strat/mwave/MwaveApplication.java
@@ -0,0 +1,14 @@
+package com.ticker.strat.mwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwaveApplication.class, args);
+	}
+
+}
---
+++ strat/mwave/src/test/java/com/ticker/strat/mwave/MwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticker.strat.mwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
