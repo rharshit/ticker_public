f9a4fac8ab2a3dcc56387a6b97c3ece4139f778b
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-20 15:38:37 +0530

    M-Wave: Move to core

--- mwave/src/main/java/com/ticker/mwave/MwaveApplication.java
+++ mwave/src/main/java/com/ticker/mwave/MwaveApplication.java
@@ -1,4 +1,4 @@
-package com.ticker.strat.mwave;
+package com.ticker.mwave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
