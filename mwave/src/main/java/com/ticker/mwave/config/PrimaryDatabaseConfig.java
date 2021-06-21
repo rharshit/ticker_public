1e234ef3ea2fb0263bd3257936f95577cfc52867
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-22 01:54:25 +0530

    MWave: Add basic thread handling

---
+++ mwave/src/main/java/com/ticker/mwave/config/PrimaryDatabaseConfig.java
@@ -0,0 +1,41 @@
+package com.ticker.mwave.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PrimaryDatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Primary
+    @Bean
+    public DataSource getDataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(jdbcUrl);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setPoolName("HikariPoolPrimary");
+
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
+}
