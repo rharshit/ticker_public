ea0ce5c5f56c979e005d96a5f7384807493e7c01
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-22 02:43:58 +0530

    Common: Add common thread handling

---
+++ common/src/main/java/com/ticker/common/model/TickerThreadModel.java
@@ -0,0 +1,35 @@
+package com.ticker.common.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ticker.common.rx.TickerThread;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TickerThreadModel<T extends TickerThread> {
+
+    @JsonIgnore
+    private T thread;
+
+    public String getThreadName() {
+        return this.thread.getThreadName();
+    }
+
+    public boolean isEnabled() {
+        return this.thread.isEnabled();
+    }
+
+    public boolean isInitialized() {
+        return this.thread.isInitialized();
+    }
+
+    public String getExchange() {
+        return this.thread.getExchange();
+    }
+
+    public String getSymbol() {
+        return this.thread.getSymbol();
+    }
+
+}
