3f278c64888be9dd470fe404b02a87c833ad9a93
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-06-21 23:35:32 +0530

    Template: Add template

--- template/src/main/java/com/ticker/template/common/exception/TickerException.java
+++
@@ -1,7 +0,0 @@
-package com.ticker.template.common.exception;
-
-public class TickerException extends RuntimeException {
-    public TickerException(String message) {
-        super(message);
-    }
-}
