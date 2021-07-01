cefed2d8da618e257d875f7a5d820092e6c34e96
Harshit Raghuvanshi <rharshit98@gmail.com>
2021-07-01 18:45:04 +0530

    Fetcher: Refactor FetcherAppRepository

--- fetcher/src/main/java/com/ticker/fetcher/repository/FetcherAppRepository.java
+++ fetcher/src/main/java/com/ticker/fetcher/repository/FetcherAppRepository.java
@@ -2,11 +2,9 @@ package com.ticker.fetcher.repository;
 
 import com.ticker.common.exception.TickerException;
 import com.ticker.fetcher.common.repository.FetcherRepository;
-import com.ticker.fetcher.common.repository.TickerRepository;
 import com.ticker.fetcher.model.FetcherRepoModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -24,10 +22,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.ticker.fetcher.common.constants.DBConstants.*;
+import static com.ticker.fetcher.common.constants.DBConstants.TABLE_NAME;
 import static com.ticker.fetcher.common.constants.FetcherConstants.FETCHER_DATE_FORMAT_LOGGING;
 import static com.ticker.fetcher.common.constants.ProcedureConstants.ADD_TABLE;
-import static com.ticker.fetcher.common.constants.ProcedureConstants.GET_EXCHANGE_SYMBOL_ID_PR;
 
 @Repository
 @Slf4j
@@ -36,26 +33,8 @@ public class FetcherAppRepository {
     @Autowired
     FetcherRepository fetcherRepository;
 
-    @Autowired
-    TickerRepository tickerRepository;
-
     private static final List<String> sqlQueue = new ArrayList<>();
     private Connection fetcherConnection = null;
-
-    public int getExchangeSymbolId(String exchange, String symbol) {
-        SqlParameter[] sqlParameters = {
-                new SqlParameter(I_EXCHANGE_ID, Types.VARCHAR),
-                new SqlParameter(I_SYMBOL_ID, Types.VARCHAR),
-                new SqlOutParameter(O_EXCHANGE_SYMBOL_ID, Types.INTEGER)
-        };
-
-        Map<String, Object> inputParameters = new HashMap<>();
-        inputParameters.put(I_EXCHANGE_ID, exchange);
-        inputParameters.put(I_SYMBOL_ID, symbol);
-
-        Map<String, Object> result = tickerRepository.executeProcedure(GET_EXCHANGE_SYMBOL_ID_PR, sqlParameters, inputParameters);
-        return (int) result.get(O_EXCHANGE_SYMBOL_ID);
-    }
 
     public void addTable(String tableName) {
         try {
