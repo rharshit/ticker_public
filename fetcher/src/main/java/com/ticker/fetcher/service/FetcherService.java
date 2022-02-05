3c88cbbeae8a747e88674b268d0d0478b55b4f11
Harshit Raghuvanshi <rharshit98@gmail.com>
2022-02-06 04:16:41 +0530

    Fetcher: Optimize task execution

--- fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
+++ fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
@@ -187,160 +187,177 @@ public class FetcherService {
         List<FetcherThread> pool = appService.getCurrentTickerList();
         for (FetcherThread thread : pool) {
             try {
-                fetcherTaskExecutor.execute(() -> doTask(thread));
+                synchronized (thread) {
+                    if (!thread.isTaskStarted()) {
+                        log.debug("Starting task: " + thread.getThreadName());
+                        fetcherTaskExecutor.execute(() -> doTask(thread));
+                        thread.setTaskStarted(true);
+                    } else {
+                        log.trace("Task already started: " + thread.getThreadName());
+                    }
+                }
             } catch (Exception e) {
+                log.error("Error in starting task: " + thread.getThreadName());
                 log.error(thread == null ? "" : (thread.getThreadName() + " : ") + e.getMessage());
             }
         }
     }
 
     public void doTask(FetcherThread fetcherThread) {
-        log.debug("doTask start: " + fetcherThread.getThreadName());
+        if (!fetcherThread.isEnabled()) {
+            log.info("Terminating fetch task: " + fetcherThread.getThreadName());
+            return;
+        }
+        boolean fetched = false;
         try {
-            if (!fetcherThread.isLocked()) {
-                fetcherThread.setLocked(true);
-                if (fetcherThread.isInitialized() && fetcherThread.isEnabled()) {
-                    try { // Get current value
-                        String title = fetcherThread.getWebDriver().getTitle();
-                        for (String text : title.split(" ")) {
-                            if (Pattern.matches("^\\d*\\.\\d*$", text)) {
-                                fetcherThread.setCurrentValue(Float.parseFloat(text));
-                            }
+            log.debug("doTask start: " + fetcherThread.getThreadName());
+            if (fetcherThread.isInitialized() && fetcherThread.isEnabled()) {
+                try { // Get current value
+                    String title = fetcherThread.getWebDriver().getTitle();
+                    for (String text : title.split(" ")) {
+                        if (Pattern.matches("^\\d*\\.\\d*$", text)) {
+                            fetcherThread.setCurrentValue(Float.parseFloat(text));
                         }
-                    } catch (Exception ignored) {
                     }
-                    try { // Get OHLC Value
-                        log.debug("doTask() started task: " + fetcherThread.getThreadName());
-                        List<String> texts;
-                        fetcherThread.getWebDriver().switchTo().window(fetcherThread.getWebDriver().getWindowHandle());
-                        WebElement table = fetcherThread.getWebDriver()
-                                .findElement(By.cssSelector("table[class='chart-markup-table']"));
-                        Rectangle hoverBox = table.getRect();
-                        Actions actions = new Actions(fetcherThread.getWebDriver());
-                        actions.moveToElement(table).perform();
-                        actions.moveByOffset(hoverBox.width / 2 - 1, 1 - (hoverBox.height / 2)).perform();
-                        waitFor(WAIT_SHORT);
-                        List<WebElement> rows = table.findElements(By.tagName("tr"));
-                        texts = rows.stream().map(WebElement::getText).collect(Collectors.toList());
-                        float o = 0;
-                        float h = 0;
-                        float l = 0;
-                        float c = 0;
-                        float bbA = 0;
-                        float bbU = 0;
-                        float bbL = 0;
-                        float rsi = 0;
-                        float tema = 0;
-                        String ohlcbbRowText = texts.get(0);
-                        if (!StringUtils.isEmpty(ohlcbbRowText)) {
-                            String[] vals = ohlcbbRowText.split("\n");
-                            float tmpVal = 0;
-                            for (int i = 0; i < vals.length && tmpVal == 0; i++) {
-                                int io = vals[i].indexOf("O");
-                                int ih = vals[i].indexOf("H");
-                                int il = vals[i].indexOf("L");
-                                int ic = vals[i].indexOf("C");
-                                if (o * h * l * c == 0 && (io < ih && ih < il && il < ic)) {
-                                    String val = vals[i];
+                } catch (Exception ignored) {
+                }
+                try { // Get OHLC Value
+                    log.debug("doTask() started task: " + fetcherThread.getThreadName());
+                    List<String> texts;
+                    fetcherThread.getWebDriver().switchTo().window(fetcherThread.getWebDriver().getWindowHandle());
+                    WebElement table = fetcherThread.getWebDriver()
+                            .findElement(By.cssSelector("table[class='chart-markup-table']"));
+                    Rectangle hoverBox = table.getRect();
+                    Actions actions = new Actions(fetcherThread.getWebDriver());
+                    actions.moveToElement(table).perform();
+                    actions.moveByOffset(hoverBox.width / 2 - 1, 1 - (hoverBox.height / 2)).perform();
+                    waitFor(WAIT_SHORT);
+                    List<WebElement> rows = table.findElements(By.tagName("tr"));
+                    texts = rows.stream().map(WebElement::getText).collect(Collectors.toList());
+                    float o = 0;
+                    float h = 0;
+                    float l = 0;
+                    float c = 0;
+                    float bbA = 0;
+                    float bbU = 0;
+                    float bbL = 0;
+                    float rsi = 0;
+                    float tema = 0;
+                    String ohlcbbRowText = texts.get(0);
+                    if (!StringUtils.isEmpty(ohlcbbRowText)) {
+                        String[] vals = ohlcbbRowText.split("\n");
+                        float tmpVal = 0;
+                        for (int i = 0; i < vals.length && tmpVal == 0; i++) {
+                            int io = vals[i].indexOf("O");
+                            int ih = vals[i].indexOf("H");
+                            int il = vals[i].indexOf("L");
+                            int ic = vals[i].indexOf("C");
+                            if (o * h * l * c == 0 && (io < ih && ih < il && il < ic)) {
+                                String val = vals[i];
 
-                                    o = Float.parseFloat(val.substring(io + 1, ih).trim());
-                                    h = Float.parseFloat(val.substring(ih + 1, il).trim());
-                                    l = Float.parseFloat(val.substring(il + 1, ic).trim());
-                                    c = Float.parseFloat(val.substring(ic + 1, ic + val.substring(ic).indexOf(".") + 3).trim());
+                                o = Float.parseFloat(val.substring(io + 1, ih).trim());
+                                h = Float.parseFloat(val.substring(ih + 1, il).trim());
+                                l = Float.parseFloat(val.substring(il + 1, ic).trim());
+                                c = Float.parseFloat(val.substring(ic + 1, ic + val.substring(ic).indexOf(".") + 3).trim());
 
-                                    fetcherThread.setO(o);
-                                    fetcherThread.setH(h);
-                                    fetcherThread.setL(l);
-                                    fetcherThread.setC(c);
+                                fetcherThread.setO(o);
+                                fetcherThread.setH(h);
+                                fetcherThread.setL(l);
+                                fetcherThread.setC(c);
 
-                                    log.debug("1OHLC at " + i);
-                                } else if (bbL * bbA * bbU == 0 && "BB".equalsIgnoreCase(vals[i])) {
-                                    bbA = Float.parseFloat(vals[i + 2]);
-                                    bbU = Float.parseFloat(vals[i + 3]);
-                                    bbL = Float.parseFloat(vals[i + 4]);
+                                log.debug("1OHLC at " + i);
+                            } else if (bbL * bbA * bbU == 0 && "BB".equalsIgnoreCase(vals[i])) {
+                                bbA = Float.parseFloat(vals[i + 2]);
+                                bbU = Float.parseFloat(vals[i + 3]);
+                                bbL = Float.parseFloat(vals[i + 4]);
 
-                                    fetcherThread.setBbA(bbA);
-                                    fetcherThread.setBbU(bbU);
-                                    fetcherThread.setBbL(bbL);
+                                fetcherThread.setBbA(bbA);
+                                fetcherThread.setBbU(bbU);
+                                fetcherThread.setBbL(bbL);
 
-                                    log.debug("1BB   at " + i);
+                                log.debug("1BB   at " + i);
 
-                                } else if (tema == 0 && "TEMA".equalsIgnoreCase(vals[i])) {
-                                    tema = Float.parseFloat(vals[i + 2]);
+                            } else if (tema == 0 && "TEMA".equalsIgnoreCase(vals[i])) {
+                                tema = Float.parseFloat(vals[i + 2]);
 
-                                    fetcherThread.setTema(tema);
+                                fetcherThread.setTema(tema);
 
-                                    log.debug("1TEMA at " + i);
-                                }
-                                tmpVal = o * h * l * c * bbA * bbL * bbU * tema;
+                                log.debug("1TEMA at " + i);
+                            }
+                            tmpVal = o * h * l * c * bbA * bbL * bbU * tema;
+                        }
+                    }
+
+                    String rsiText = texts.get(4);
+                    if (!StringUtils.isEmpty(rsiText)) {
+                        String[] vals = rsiText.split("\n");
+                        for (int i = 0; i < vals.length && rsi == 0; i++) {
+                            if ("RSI".equalsIgnoreCase(vals[i])) {
+                                rsi = Float.parseFloat(vals[i + 2]);
+
+                                fetcherThread.setRsi(rsi);
+
+                                log.debug("1RSI  at 4");
                             }
                         }
-
-                        String rsiText = texts.get(4);
-                        if (!StringUtils.isEmpty(rsiText)) {
-                            String[] vals = rsiText.split("\n");
-                            for (int i = 0; i < vals.length && rsi == 0; i++) {
-                                if ("RSI".equalsIgnoreCase(vals[i])) {
-                                    rsi = Float.parseFloat(vals[i + 2]);
-
-                                    fetcherThread.setRsi(rsi);
-
-                                    log.debug("1RSI  at 4");
-                                }
-                            }
+                    }
+                    float valCheck = o * h * l * c * bbL * bbA * bbU * rsi * tema;
+                    if (valCheck == 0) {
+                        log.error(fetcherThread.getThreadName() + " :\n" +
+                                o + "," + h + "," + l + "," + c + "\n"
+                                + bbL + "," + bbA + "," + bbU + "\n"
+                                + rsi);
+                    } else {
+                        log.trace(fetcherThread.getThreadName() + " :\n" +
+                                o + "," + h + "," + l + "," + c + "\n"
+                                + bbL + "," + bbA + "," + bbU + "\n"
+                                + rsi + "," + tema);
+                        synchronized (dataQueue) {
+                            dataQueue.add(new FetcherRepoModel(fetcherThread.getTableName(), System.currentTimeMillis(),
+                                    o, h, l, c, bbU, bbA, bbL, rsi, tema));
                         }
-                        float valCheck = o * h * l * c * bbL * bbA * bbU * rsi * tema;
-                        if (valCheck == 0) {
-                            log.error(fetcherThread.getThreadName() + " :\n" +
-                                    o + "," + h + "," + l + "," + c + "\n"
-                                    + bbL + "," + bbA + "," + bbU + "\n"
-                                    + rsi);
-                        } else {
-                            log.trace(fetcherThread.getThreadName() + " :\n" +
-                                    o + "," + h + "," + l + "," + c + "\n"
-                                    + bbL + "," + bbA + "," + bbU + "\n"
-                                    + rsi + "," + tema);
-                            synchronized (dataQueue) {
-                                dataQueue.add(new FetcherRepoModel(fetcherThread.getTableName(), System.currentTimeMillis(),
-                                        o, h, l, c, bbU, bbA, bbL, rsi, tema));
-                            }
-                            log.debug("doTask() added data: " + fetcherThread.getThreadName() + ", size: " + dataQueue.size());
-                        }
+                        fetched = true;
+                        log.debug("doTask() added data: " + fetcherThread.getThreadName() + ", size: " + dataQueue.size());
+                    }
 
-                    } catch (IndexOutOfBoundsException | NumberFormatException e) {
-                        log.debug(e.getClass().getName());
-                        StackTraceElement[] stackTraceElements = e.getStackTrace();
-                        for (StackTraceElement stackTraceElement : stackTraceElements) {
-                            log.debug("\t" + stackTraceElement.toString());
-                        }
-                        log.info(fetcherThread.getThreadName() + " : " + e.getMessage());
-                    } catch (NoSuchSessionException e) {
-                        log.error("Destroying: " + fetcherThread.getThreadName());
-                        fetcherThread.destroy();
+                } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                    log.debug(e.getClass().getName());
+                    StackTraceElement[] stackTraceElements = e.getStackTrace();
+                    for (StackTraceElement stackTraceElement : stackTraceElements) {
+                        log.debug("\t" + stackTraceElement.toString());
+                    }
+                    log.info(fetcherThread.getThreadName() + " : " + e.getMessage());
+                } catch (NoSuchSessionException e) {
+                    log.error("Destroying: " + fetcherThread.getThreadName());
+                    fetcherThread.destroy();
+                    throw e;
+                } catch (Exception e) {
+                    String errorMessage = e.getMessage();
+                    if (errorMessage.contains("element click intercepted")) {
+                        log.info("Element click intercepted: " + fetcherThread.getThreadName());
+                    } else if (errorMessage.contains("move target out of bounds")) {
+                        log.info("Move target out of bounds: " + fetcherThread.getThreadName());
+                    } else {
+                        log.error("Exception in doTask(): " + fetcherThread.getThreadName());
+                        log.error(e.getMessage());
                         throw e;
-                    } catch (Exception e) {
-                        String errorMessage = e.getMessage();
-                        if (errorMessage.contains("element click intercepted")) {
-                            log.info("Element click intercepted: " + fetcherThread.getThreadName());
-                        } else if (errorMessage.contains("move target out of bounds")) {
-                            log.info("Move target out of bounds: " + fetcherThread.getThreadName());
-                        } else {
-                            log.error("Exception in doTask(): " + fetcherThread.getThreadName());
-                            log.error(e.getMessage());
-                            throw e;
-                        }
                     }
-                } else {
-                    log.debug("Skipping doTask()");
                 }
             } else {
-                log.debug(fetcherThread.getThreadName() + " : Thread locked");
+                log.debug("Skipping doTask()");
             }
 
         } finally {
-            fetcherThread.setLocked(false);
+            long timeToWait = fetcherThread.isInitialized() ? (fetched ? 250 : 100) : 2500;
+            try {
+                Thread.sleep(timeToWait);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.debug("Adding task to executor: " + fetcherThread.getThreadName());
+            fetcherTaskExecutor.execute(() -> doTask(fetcherThread));
+            log.debug("doTask end: " + fetcherThread.getThreadName());
         }
-        log.debug("doTask end: " + fetcherThread.getThreadName());
     }
 
     @Async("scheduledExecutor")
