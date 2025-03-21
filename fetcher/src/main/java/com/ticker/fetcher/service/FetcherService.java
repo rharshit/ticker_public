5d8013fa463637b0c23d85b434cd48edaf9f43df
Harshit Raghuvanshi <rharshit98@gmail.com>
2025-03-21 15:10:18 +0530

    Fetcher: Optimize websockets for fetching

--- fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
+++ fetcher/src/main/java/com/ticker/fetcher/service/FetcherService.java
@@ -82,6 +82,8 @@ public class FetcherService extends BaseService {
      */
     public void sendMessage(WebSocketClient webSocket, String data) {
         if (webSocket != null && webSocket.isOpen()) {
+            log.trace("Sending message:");
+            log.trace(data);
             webSocket.send(encodeMessage(data));
             waitFor(5);
         }
@@ -96,17 +98,9 @@ public class FetcherService extends BaseService {
      * @param temp
      */
     public void onReceiveMessage(FetcherThread thread, WebSocketClient webSocket, String data, boolean temp) {
-        log.debug("Recv:");
+        log.trace("Recv:");
         String[] messages = decodeMessage(data);
         for (String message : messages) {
-            String parsedMessage = message;
-            try {
-                JSONObject jsonObject = new JSONObject(message);
-                parsedMessage = jsonObject.toString(2);
-            } catch (Exception e) {
-
-            }
-            log.debug("\n" + parsedMessage);
             if (Pattern.matches("~h~\\d*$", message)) {
                 if (!temp) {
                     sendMessage(webSocket, message);
@@ -142,10 +136,7 @@ public class FetcherService extends BaseService {
     }
 
     private boolean hasInterestedValue(FetcherThread thread, JSONObject jsonObject) {
-        if (jsonObject.has(thread.getStudySeries())
-                || jsonObject.has(thread.getStudyBB())
-                || jsonObject.has(thread.getStudyRSI())
-                || jsonObject.has(thread.getStudyTEMA())) {
+        if (jsonObject.has(thread.getStudySeries())) {
             return true;
         }
         if (jsonObject.has("v")) {
@@ -159,7 +150,8 @@ public class FetcherService extends BaseService {
     }
 
     private void setVal(FetcherThread thread, JSONObject object, boolean temp) {
-        log.trace(thread.getThreadName() + " : Setting value");
+        log.debug(thread.getThreadName() + " : Setting value");
+        log.debug(object.toString(2));
         for (String key : object.keySet()) {
             log.trace("Key: " + key);
             try {
@@ -180,17 +172,6 @@ public class FetcherService extends BaseService {
                     thread.setH(vals[2]);
                     thread.setL(vals[3]);
                     thread.setC(vals[4]);
-                } else if (thread.getStudyBB().equals(key)) {
-                    log.trace(thread.getThreadName() + " : Setting BB value");
-                    thread.setBbA(vals[1]);
-                    thread.setBbU(vals[2]);
-                    thread.setBbL(vals[3]);
-                } else if (thread.getStudyRSI().equals(key)) {
-                    log.trace(thread.getThreadName() + " : Setting RSI value");
-                    thread.setRsi(vals[1]);
-                } else if (thread.getStudyTEMA().equals(key)) {
-                    log.trace(thread.getThreadName() + " : Setting TEMA value");
-                    thread.setTema(vals[1]);
                 } else if ("v".equals(key)) {
                     log.trace(thread.getThreadName() + " : Setting day values value");
                     JSONObject value = object.getJSONObject("v");
@@ -320,70 +301,34 @@ public class FetcherService extends BaseService {
         }
         if (thread.isEnabled()) {
             sendMessagesForInitializing(webSocket, thread);
-            sendMessagesForModifyingCharts(webSocket, thread);
-            sendMessagesForAddingStudies(webSocket, thread);
-
             log.debug("Sent messages");
 
         }
     }
 
     private void sendMessagesForInitializing(WebSocketClient webSocket, FetcherThread thread) {
+        log.debug("{} : Creating websocket sessions", thread.getThreadName());
         sendMessage(webSocket, "{\"m\":\"set_auth_token\",\"p\":[\"unauthorized_user_token\"]}");
+        sendMessage(webSocket, "{\"m\":\"set_locale\",\"p\":[\"en\",\"IN\"]}");
         sendMessage(webSocket, "{\"m\":\"chart_create_session\",\"p\":[\"" + thread.getChartSession() + "\",\"\"]}");
         sendMessage(webSocket, "{\"m\":\"quote_create_session\",\"p\":[\"" + thread.getQuoteSession() + "\"]}");
         waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"quote_set_fields\",\"p\":[\"" + thread.getQuoteSession() + "\",\"base-currency-logoid\",\"ch\",\"chp\",\"currency-logoid\",\"currency_code\",\"current_session\",\"description\",\"exchange\",\"format\",\"fractional\",\"is_tradable\",\"language\",\"local_description\",\"logoid\",\"lp\",\"lp_time\",\"minmov\",\"minmove2\",\"original_name\",\"pricescale\",\"pro_name\",\"short_name\",\"type\",\"update_mode\",\"volume\",\"rchp\",\"rtc\",\"country_code\",\"provider_id\"]}");
+        log.debug("{} : Adding symbols to websocket", thread.getThreadName());
         sendMessage(webSocket, "{\"m\":\"request_studies_metadata\",\"p\":[\"" + thread.getChartSession() + "\",\"metadata_1\"]}");
+        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSession() + "\",\"={\\\"adjustment\\\":\\\"splits\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\"}\"]}");
+        sendMessage(webSocket, "{\"m\":\"quote_fast_symbols\",\"p\":[\"" + thread.getQuoteSession() + "\",\"={\\\"adjustment\\\":\\\"splits\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\"}\"]}");
         waitFor(WAIT_QUICK);
+        log.debug("{} : Resolving symbol", thread.getThreadName());
+        sendMessage(webSocket, "{\"m\":\"switch_timezone\",\"p\":[\"" + thread.getChartSession() + "\",\"Asia/Kolkata\"]}");
         sendMessage(webSocket, "{\"m\":\"resolve_symbol\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_sym_1\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
         waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"create_series\",\"p\":[\"" + thread.getChartSession() + "\",\"" + thread.getStudySeries() + "\",\"s1\",\"sds_sym_1\",\"D\",300,\"\"]}");
-        sendMessage(webSocket, "{\"m\":\"switch_timezone\",\"p\":[\"" + thread.getChartSession() + "\",\"Asia/Kolkata\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_create_session\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\"]}");
+        log.debug("{} : Creating series", thread.getThreadName());
+        sendMessage(webSocket, "{\"m\":\"create_series\",\"p\":[\"" + thread.getChartSession() + "\",\"" + thread.getStudySeries() + "\",\"s1\",\"sds_sym_1\",\"1\",300,\"\"]}");
         waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"st1\",\"st1\",\"" + thread.getStudySeries() + "\",\"Dividends@tv-basicstudies-149\",{}]}");
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"st2\",\"st1\",\"" + thread.getStudySeries() + "\",\"Splits@tv-basicstudies-149\",{}]}");
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"st3\",\"st1\",\"" + thread.getStudySeries() + "\",\"Earnings@tv-basicstudies-149\",{}]}");
-        sendMessage(webSocket, "{\"m\":\"quote_create_session\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_set_fields\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\",\"base-currency-logoid\",\"ch\",\"chp\",\"currency-logoid\",\"currency_code\",\"current_session\",\"description\",\"exchange\",\"format\",\"fractional\",\"is_tradable\",\"language\",\"local_description\",\"logoid\",\"lp\",\"lp_time\",\"minmov\",\"minmove2\",\"original_name\",\"pricescale\",\"pro_name\",\"short_name\",\"type\",\"update_mode\",\"volume\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_fast_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_remove_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_fast_symbols\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\"]}");
+        log.debug("{} : Requesting more ticks", thread.getThreadName());
         sendMessage(webSocket, "{\"m\":\"request_more_tickmarks\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_1\",10]}");
         waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"st4\",\"st1\",\"sds_1\",\"Volume@tv-basicstudies-149\",{\"length\":20,\"col_prev_close\":false}]}");
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_remove_symbols\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_fast_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\"}\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_hibernate_all\",\"p\":[\"" + thread.getQuoteSessionTickerNew() + "\"]}");
-        waitFor(WAIT_QUICK);
-    }
-
-    private void sendMessagesForModifyingCharts(WebSocketClient webSocket, FetcherThread thread) {
-        sendMessage(webSocket, "{\"m\":\"resolve_symbol\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_sym_2\",\"={\\\"symbol\\\":{\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\"},\\\"type\\\":\\\"BarSetHeikenAshi@tv-basicstudies-60!\\\",\\\"inputs\\\":{}}\"]}");
-        sendMessage(webSocket, "{\"m\":\"modify_series\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_1\",\"s2\",\"sds_sym_2\",\"D\",\"\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_remove_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\",\\\"session\\\":\\\"regular\\\"}\"]}");
-        waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"resolve_symbol\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_sym_3\",\"={\\\"symbol\\\":{\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\",\\\"session\\\":\\\"regular\\\"},\\\"type\\\":\\\"BarSetHeikenAshi@tv-basicstudies-60!\\\",\\\"inputs\\\":{}}\"]}");
-        sendMessage(webSocket, "{\"m\":\"modify_series\",\"p\":[\"" + thread.getChartSession() + "\",\"sds_1\",\"s3\",\"sds_sym_3\",\"1\",\"\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_remove_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_add_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\",\\\"session\\\":\\\"regular\\\"}\"]}");
-        sendMessage(webSocket, "{\"m\":\"quote_fast_symbols\",\"p\":[\"" + thread.getQuoteSessionTicker() + "\",\"" + thread.getExchange() + ":" + thread.getSymbol() + "\",\"={\\\"symbol\\\":\\\"" + thread.getExchange() + ":" + thread.getSymbol() + "\\\",\\\"currency-id\\\":\\\"INR\\\",\\\"adjustment\\\":\\\"splits\\\",\\\"session\\\":\\\"regular\\\"}\"]}");
-        waitFor(WAIT_QUICK);
-    }
-
-    private void sendMessagesForAddingStudies(WebSocketClient webSocket, FetcherThread thread) {
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"" + thread.getStudyBB() + "\",\"st1\",\"sds_1\",\"Script@tv-scripting-101!\",{\"text\":\"zW1+wVOc52/yPnU4LWhyTw==_htNzUrc457Lyx9myW31mZZKXTme5DvaQUPCkw82+J6qvgURkPjRtam7d09ED0nuuEjGHKncBMo2MkN/YUUk1DUZAfFeYIbzm4UdSxVKs6nXa/Wc181Ai+nThySLOr7mi6sdkdSPxpg3yx1kWeTsNORKWjXvcJszgeMjQq69LtbzV35Qg+fIi+4MI8jzVoGn1KeQGltl0pBVvZmdC8zL+W1RGCtfNz8IFIHo2i2OjQlX5zza2JQNZVBDQ6EFeAVmIm0HlK63Q31NsbT5pFPMCSAmK9TQ5c/5fkiBNnQP6UjYAs5j6NwTSr7RSx9un5viiyXa0EmFlYteP2lDJ8zXSKnmxPt+H7CtPKHp0PyYI8s1ySA3wR5Kt5oKK+ggNheJNW40tS3Y3BEk5rDqNnSSDJJzIOE3+yrXIfZG0/5Dot9nvrULz2Pnu69YaipCWcUtNT1bFr6TIAKXo7BF7M1q2m3t5BlmO6bpi5AcT5GBNPgCIISyDRtT80LB8EiDlaiMsDUUFqmrTgQqDb2Z3pw==\",\"pineId\":\"STD;Bollinger_Bands\",\"pineVersion\":\"28.0\",\"in_4\":{\"v\":\"\",\"f\":true,\"t\":\"resolution\"},\"in_5\":{\"v\":true,\"f\":true,\"t\":\"bool\"},\"in_0\":{\"v\":20,\"f\":true,\"t\":\"integer\"},\"in_1\":{\"v\":\"close\",\"f\":true,\"t\":\"source\"},\"in_2\":{\"v\":2,\"f\":true,\"t\":\"float\"},\"in_3\":{\"v\":0,\"f\":true,\"t\":\"integer\"}}]}");
-        waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"" + thread.getStudyRSI() + "\",\"st1\",\"sds_1\",\"Script@tv-scripting-101!\",{\"text\":\"p29QEj9lAJh9iuw+VOP65Q==_Rneggp8b01hBqOJXEivQHXTJxITYPEFECf2EFl4KXWSRaVE9SwE8Iovc4biw4RLHMIWzg03VnVR8FkKiFBe+L3OJX9y115b0aLBmFLPfKHdmerSijtt6B2RkhbAF9T7tSoo1Kj64RuZPoCl5IOxcfwsTMINLvIDZBvWTVouniXdzJbCQzKkfj5D1Vnc14+SSf97QaTFMzwqNcuLNVbcAY3KgoPTtMh3kJnk4kdiJk8C2gR2NxEOcX0MrT5fvXUf+hRXgcdUK5vsn8bhoB7CYOP/c8K6krOVK3cCs6NrySxkywK7leUxaNujhXz5AM82pUxrEB0DpVKZ55WwLISRb4l48N6rnqi2WPjUkH7Y9AijwAJIxan58rjTBh32IRkjA1Idc0+KYv7qUlJ2fErWMJgRwLb5i45Gbu10lyJKHcRg021FcUPVWduxaeyPUXVr+LmUjdRsnzTpSFg1VyaYyEF9ERT4gEPXTQqtVCFqEFesxmY7mVLL7Pzj1hXAlAeOtDA5MF6ts6h2Pfw2KXZo5yd0OBxtk0XS8njtg4fQJDJMNdCsQLukGcZY898h8ZCUcQZQcik3JZ+B38ImM+sTllJiltQUVMBFqPy6rmYCciwXoN9NTuPc5OrNbZwafvL3QYWGWAGtJzHZkNJ5VsIrjnmPCj1+3G8DIfI+NB0pbmPbyRLn0VzM6gSDpIlYgkLIYoPIuXPuFR55T0H5hUyMGzT6XtbnH5OvZShKzl4dWevRHTsfFxJmigyT33ORTMcIwuDLjmzICBujtInU6tqN8KkR+SZb5GUwKaM9RFuTLei2IX/kOkPFvwypBYf1QM+JMCMHSR6d+20zOmi3VNg4I3cWAwYaUkdwYXxSKwewJBloYz9CZ51erSA+saYwupXDW79LDePV5Cfgp/fai03sbvgvS8vVtmo+Ergl7g1fSCV2RRiahSq/n9iig1SCUSQNUEZGWL5AnC4/beCqzFpqVsg5a1eJMM8EPIZkI9fZDjguTu68=\",\"pineId\":\"STD;RSI\",\"pineVersion\":\"30.0\",\"in_5\":{\"v\":\"\",\"f\":true,\"t\":\"resolution\"},\"in_6\":{\"v\":true,\"f\":true,\"t\":\"bool\"},\"in_0\":{\"v\":14,\"f\":true,\"t\":\"integer\"},\"in_1\":{\"v\":\"close\",\"f\":true,\"t\":\"source\"},\"in_2\":{\"v\":\"SMA\",\"f\":true,\"t\":\"text\"},\"in_3\":{\"v\":14,\"f\":true,\"t\":\"integer\"},\"in_4\":{\"v\":2,\"f\":true,\"t\":\"float\"}}]}");
-        waitFor(WAIT_QUICK);
-        sendMessage(webSocket, "{\"m\":\"create_study\",\"p\":[\"" + thread.getChartSession() + "\",\"" + thread.getStudyTEMA() + "\",\"st1\",\"sds_1\",\"Script@tv-scripting-101!\",{\"text\":\"cwAfIpmLfrGt8bNrgB2hCw==_+Qla9wmHqX4jzJGVyTxURoQNsSjSwdKXChidtfJpyRVvFSiDzg7UW2cSBrsvApBG56uEjDtuCW6u1rJbqIUzIY1ueGe07kXIZ0Z3BVQCsQP0P9lTaubuMcVAVG/xItX3qwIP5o8V7q5NQWbEhYSfMaC73AiBSp8gxgkm7oAliBrSgLY35rG28ll8Z7b1YYu/skzDaksloOhqBV+as7dnQCLLehsU1wZ47fpS4QzV7pERl78mMDIC85zLS1bbqq5h7wokwA7zdI/Z480vIK67uMVzPN1Q9u3X4f2SZsgjOniMYSs=\",\"pineId\":\"STD;TEMA\",\"pineVersion\":\"28.0\",\"in_1\":{\"v\":\"\",\"f\":true,\"t\":\"resolution\"},\"in_2\":{\"v\":true,\"f\":true,\"t\":\"bool\"},\"in_0\":{\"v\":9,\"f\":true,\"t\":\"integer\"}}]}");
-        waitFor(WAIT_QUICK);
+        log.debug("{} : Websocket initialization messages sent", thread.getThreadName());
     }
 
     /**
